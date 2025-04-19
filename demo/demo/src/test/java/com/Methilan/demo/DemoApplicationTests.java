2894fa82911da445f70c7d2adbd03afd573c7564
Methilan-R <rmethilan@gmail.com>
2025-04-19 09:17:12 +0530

    new spring

---
+++ demo/demo/src/test/java/com/Methilan/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Methilan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
